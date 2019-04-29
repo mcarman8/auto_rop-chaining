f23d4d7ba0c2fb4129a8507a31b7eb0e2940488a
mcarman8 <mcarman8@vt.edu>
2019-04-29 13:24:46 -0400

    Added some comments and command line args

--- find_all_gadgets.py
+++ find_all_gadgets.py
@@ -8,8 +8,9 @@ import sys
 from StringIO import StringIO
 import argparse
 
-def create_gadgets(proj,instruction_addrs,all_gadgets_file):
-    for i in range (1,12):
+#This function generates the gadgets from a return location
+def create_gadgets(proj,instruction_addrs,all_gadgets_file,the_range):
+    for i in range (1,the_range):
         old_stdout = sys.stdout
         result = StringIO()
         sys.stdout = result
@@ -21,15 +22,19 @@ def create_gadgets(proj,instruction_addrs,all_gadgets_file):
             all_gadgets_file.write(result_string)
             all_gadgets_file.write("\n--------------------------------------------------------------------------\n")
 
-
+#This gets the arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("file")
+parser.add_argument("file",help="The file in which you want to find ROP gadgets")
+parser.add_argument("-n",type=int,help="Number of bytes to use to generate intermediate gadgets. Default is 12",default=12)
 args = parser.parse_args()
+#set up the angr project
 proj = angr.Project(args.file)
 cfg = proj.analyses.CFGFast()
 entry_func = cfg.kb.functions[proj.entry]
 items = proj.kb.functions.items()
+#File for output
 all_gadgets_file = open("./all_gadgets.txt","w+")
+#for every section that angr creates
 for item in items:
     block = proj.factory.block(item[0])
     old_stdout = sys.stdout
@@ -38,5 +43,6 @@ for item in items:
     block.pp()
     sys.stdout = old_stdout
     result_string = result.getvalue()
+    #If there is a return, we can generate rop gadgets
     if "ret" in result_string:
-        create_gadgets(proj,block.instruction_addrs,all_gadgets_file)
+        create_gadgets(proj,block.instruction_addrs,all_gadgets_file,args.n)
